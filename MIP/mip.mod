50f76edebd23eb38471bf4a72704e09e6d409375
Scheggetta <edoardo.fusa@gmail.com>
2023-12-07 12:00:27 +0100

    Bounds fix on MIP model

--- MIP/mip.mod
+++ MIP/mip.mod
@@ -9,20 +9,14 @@ param C{COURIERS} integer >= 0; # capacity of vehicles
 param L{NODES} integer >= 0; # loads of items
 param D{NODES_1, NODES_1} integer >= 0; # weights of the arcs
 
-param lb integer >= 0; # lower bound for the objective function
-param ub integer >= 0; # upper bound for the objective function
-
 var x{NODES_1, NODES_1, COURIERS} in {0..1}; # Edges tensor
 var y{NODES_1, COURIERS} in {0..1}; # customers matrix
 # var u{NODES_1, COURIERS} integer >= 0; # Cumulative load delivered by couriers
 var u{NODES} integer >= 0; # Cumulative load delivered by couriers
 var objective integer >= 0; # Objective function
 
-# minimize cost_function : max{k in COURIERS} sum {i in NODES_1, j in NODES_1} D[i,j]*x[i,j,k];
-minimize cost_function: objective;
-subject to bounds: lb <= objective <= ub;
+minimize cost_function : max{k in COURIERS} sum {i in NODES_1, j in NODES_1} D[i,j]*x[i,j,k];
 
-s.t. objective_assignement: objective == max{k in COURIERS} sum {i in NODES_1, j in NODES_1} D[i,j]*x[i,j,k];
 s.t. self_loops{i in NODES_1, k in COURIERS}: x[i,i,k] == 0;
 s.t. exactly_once {i in NODES}: sum {k in COURIERS} y[i,k] == 1;
 s.t. depot_k: sum {k in COURIERS} y[N+1, k] == K;
--- MIP/mip.py
+++ MIP/mip.py
@@ -29,30 +29,16 @@ def mip_model(instance_file: str, instance_number: str, solver: str, time_limit:
 	ampl_solver.set_option("times", 0)
 
 	ampl_solver.read(os.path.join(module_path, "mip.mod"))
-	ampl_solver.get_parameter("lb").set_values([lb])
-	ampl_solver.get_parameter("ub").set_values([ub])
-
-	ampl_solver.read_data(instance_file)
-	ampl_solver.set_option("solver", solver)
-
-	# n = ampl_solver.get_parameter('N').value()
-	# m = ampl_solver.get_parameter('K').value()
-	# D = ampl_solver.get_parameter('D').to_list()
-	# d = [[0 for _ in range(n+1)] for _ in range(n+1)]
-	# for row, col, val in D:
-	# 	d[row-1][col-1] = val
-	# lb = generate_lowerbound(n, d)
-	# ub = generate_upperbound(n, m, d)
-
-	#ampl_solver.eval("s.t. lower_bound: max{k in COURIERS} sum {i in NODES_1, j in NODES_1} D[i,j]*x[i,j,k] >="+str(lb)+";")
-	#ampl_solver.eval("s.t. upper_bound: max{k in COURIERS} sum {i in NODES_1, j in NODES_1} D[i,j]*x[i,j,k] <="+str(ub)+";")
-
-
 	if sym_break:
 		ampl_solver.eval(
 			"s.t. sym_break {k in COURIERS, m in COURIERS, j in NODES: k<m}: max(sum{i in NODES} y[i,k]*L[i], "
 			"sum{i in NODES} y[i,m]*L[i]) <= min(C[k], C[m]) ==> ((x[N+1, j, k] == 1) ==> sum{l in {1..j}} x[N+1, l, m] == 0);"
 		)
+	ampl_solver.eval("s.t. lower_bound: max{k in COURIERS} sum {i in NODES_1, j in NODES_1} D[i,j]*x[i,j,k] >="+str(lb)+";")
+	ampl_solver.eval("s.t. upper_bound: max{k in COURIERS} sum {i in NODES_1, j in NODES_1} D[i,j]*x[i,j,k] <="+str(ub)+";")
+
+	ampl_solver.read_data(instance_file)
+	ampl_solver.set_option("solver", solver)
 
 	# Each solver has its own way to set time limit example:
 	# ampl_solver.set_option('highs_options', f'time_limit={1}')
